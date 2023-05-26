17b627a54de1883d4c354571fdbda54758d2acd3
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2023-05-26 18:31:28 +0500

    swagger

--- core/settings.py
+++ core/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'debug_toolbar',
+    'drf_yasg',
     
     'user',
     'apps.body',
--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,19 @@
 # Django
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+# Project
+from core.yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('main/', include('apps.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
+
+urlpatterns += doc_urls
+
+urlpatterns += tuple(static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
+urlpatterns += tuple(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
